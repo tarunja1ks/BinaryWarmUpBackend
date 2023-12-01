cbfdb8962540ed3951cf49f4bdc802fc0c35472f
Vynz123 <shubhayc14@gmail.com>
2023-11-30 21:49:38 -0800

    FINISHED FLASK SETUP THIS TOOK SO LONG

--- flask_portfolio-main/main.py
+++ flask_portfolio-main/main.py
@@ -1,32 +1,23 @@
 import threading
-
+import sys
 # import "packages" from flask
 from flask import render_template  # import render_template from "public" flask libraries
-
+from aienglishprediction import aienglishprediction
+from flask_cors import CORS
 # import "packages" from "this" project
 from __init__ import app,db  # Definitions initialization
 
-
-# setup APIs
-from api.covid import covid_api # Blueprint import api definition
-from api.joke import joke_api # Blueprint import api definition
-from api.user import user_api # Blueprint import api definition
-from api.player import player_api
-
-
-# setup App pages
-from projects.projects import app_projects # Blueprint directory import projects definition
+from flask import Flask, render_template, request, jsonify
+from urllib.parse import quote as url_quote
+import subprocess
+import os
+from generate import generate as gn # importing the generator api
+app = Flask(__name__)
+CORS(app)
 
 
 # Initialize the SQLAlchemy object to work with the Flask app instance
-db.init_app(app)
-
-# register URIs
-app.register_blueprint(joke_api) # register api routes
-app.register_blueprint(covid_api) # register api routes
-app.register_blueprint(user_api) # register api routes
-app.register_blueprint(player_api)
-app.register_blueprint(app_projects) # register app pages
+# db.init_app(app)
 
 @app.errorhandler(404)  # catch for URL not found
 def page_not_found(e):
@@ -41,9 +32,20 @@ def index():
 def table():
     return render_template("table.html")
 
-# this runs the application on the development server
+
+@app.route("/decrypt", methods=["POST"])
+def decrypt():
+    text = request.json.get("text") # getting text
+    output="test"
+    testenglish=aienglishprediction()
+    prediction=testenglish.predict(text)
+    print(text,prediction,file=sys.stderr)
+    return jsonify(str(prediction)) # returning the output
+
+
 if __name__ == "__main__":
+    
     # change name for testing
     from flask_cors import CORS
     cors = CORS(app)
-    app.run(debug=True, host="0.0.0.0", port="8086")
+    app.run(debug=True, host="0.0.0.0", port="8080")
